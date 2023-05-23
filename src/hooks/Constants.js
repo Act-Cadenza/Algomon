154603a22c029df4f700f61c52d55ed7a212d09f
Act-Cadenza <orot.cris1@gmail.com>
2023-05-23 16:08:09 +0800

    sc update

--- src/hooks/Constants.js
+++ src/hooks/Constants.js
@@ -2,6 +2,6 @@ export const CONSTANTS = {
   algodToken: "",
   baseServer: "https://testnet-api.algonode.cloud",
   port: "443",
-  APP_ID: 214035673,
-  explorer: "https://testnet.algoexplorer.io/application/214035673",
+  APP_ID: 215750510,
+  explorer: "https://testnet.algoexplorer.io/application/215750510",
 };
